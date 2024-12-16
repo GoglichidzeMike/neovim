71bb9dab8b3354fb577b116a7632ce0de1311673
Mikael Goglichidze <goglichidze.mike@gmail.com>
2024-12-16 13:22:26 +0400

    fix: update set and lualine

--- lua/mike/set.lua
+++ lua/mike/set.lua
@@ -3,11 +3,12 @@ vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-
+            
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
@@ -30,3 +31,5 @@ vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
 
+vim.opt.ignorecase = true  -- Make searches case-insensitive...
+vim.opt.smartcase = true   -- ...unless they contain uppercase letters.
